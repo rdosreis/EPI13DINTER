415c6e5cad72b532860a4968690875439d178d1c
rdosreis <rodrigocpdosreis@gmail.com>
2019-09-24 06:16:15 -0300

    Dia 1

---
+++ RScripts/03_inferencia_causal_em_experimentos_aleatorizados.R
@@ -0,0 +1,53 @@
+## ---- echo=TRUE, eval=TRUE, message=FALSE, warning=FALSE-----------------
+# Ausência de efeito de tratamento
+r_T <- r_C <- c(rep(1, 30), rep(0, 70))
+
+# Atribuição de tratamento
+set.seed(2306)
+Z <- rbinom(n = 100, size = 1, prob = 0.5)
+
+# Desfecho observado
+R <- ifelse(Z == 1, r_T, r_C)
+round(prop.table(table(Z, R), margin = 1) * 100, 2)
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'process_mortality_outcomes_tn.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'small_example.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'small_example_results.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'small_example_t_dist.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'small_example_t_dist_resumo.png'))
+
+
+
+## ---- echo=TRUE, eval=TRUE, message=FALSE, warning=FALSE-----------------
+ProCESS <- matrix(c(92, 81, 347, 365), 2, 2)
+fisher.test(ProCESS, alternative = "greater")
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='80%', paged.print=FALSE----
+
+knitr::include_graphics(here('images', 'Fisher_colorido.png'))
+
+
